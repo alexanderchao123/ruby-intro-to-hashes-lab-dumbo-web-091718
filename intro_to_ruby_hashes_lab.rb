19d0e9220d4059ed0999469f67fecbe18fb9b2df
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-09 22:58:51 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,6 @@ def monopoly_with_third_tier
 	monopoly[:railroads][:names][:pennsylvania_railroad] = {}
 	monopoly[:railroads][:names][:b_and_o_railroad] = {}
 	monopoly[:railroads][:names][:shortline] = {}
-
 end
 
 def monopoly_with_fourth_tier
